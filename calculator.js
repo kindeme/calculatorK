48784f8e138a8ce105066cd9e1235b0eb1ab2975
Kindeme KOUNOUDJI <fkounoudji@gmail.com>
2021-11-12 08:34:37 -0600

    left arrow fixed

--- calculator.js
+++ calculator.js
@@ -8,7 +8,7 @@ buttons.map((button) => {
 			case "C":
 				display.innerText = "";
 				break;
-			case "<-":
+			case "←":
 				if (display.innerText) {
 					display.innerText = display.innerText.slice(0, -1);
 				}
